e67a6aa382a19fc3aa9edf411a209e2606f54759
施骁 <shxdev@163.com>
2019-11-28 00:37:19 +0800

    添加Usage

--- Docker科普/scripts/生成连接证书/gen-server.sh
+++ Docker科普/scripts/生成连接证书/gen-server.sh
@@ -6,22 +6,82 @@ State=Beijing
 Locality=Beijing
 Organization=SHX
 Unit=TechEdu
+CommonName=si_he_xiang.github.com
 EMAIL=
-HOST=
 
-while [ $# -ge 2 ] ; do
+Usage(){
+    cat <<EOF
+
+Usage: $0 <--pass> <--email> <[--domain][--ip]>... [OPTIONS]
+
+生成Docker使用的CA和服务端证书
+
+Requires:
+  --pass string             CA的签名密码
+  --email string            用于管理证书的邮箱,CA根证书中的"Email Address"
+  --domain string           服务器接受访问的域名(可以多个)
+  --ip string               服务器接受访问的IP地址(可以多个)
+
+Options:
+  --country string          CA根证书中的"Country Name",缺省值"${Country}"
+  --state string            CA根证书中的"State or Province Name",缺省值"${State}"
+  --locality string         CA根证书中的"Locality Name",缺省值"${Locality}"
+  --organization string     CA根证书中的"Organization Name",缺省值"${Organization}"
+  --unit string             CA根证书中的"Organizational Unit Name",缺省值"${Unit}"
+  --commonname string       CA根证书中的"Common Name",缺省值"${CommonName}"
+EOF
+    exit 1
+}
+
+command_error(){
+  echo "$1 参数值不能为空"
+  exit 1
+}
+
+if [[ "$1" == "-h" || "$1" == "--help" || -z "$1" ]]; then
+  Usage
+fi
+
+
+ACCEPT_STRING=
+ACCEPT_DOMAIN=()
+ACCEPT_IP=()
+
+while [ $# -ge 1 ] ; do
   case "$1" in
-    --country) Country="$2"; shift 2;;
-    --state) State="$2"; shift 2;;
-    --locality) Locality="$2"; shift 2;;
-    --organization) Organization="$2"; shift 2;;
-    --unit) State="$2"; shift 2;;
-    --email) EMAIL="$2"; shift 2;;
-    --pass) PASS="$2"; shift 2;;
-    --host) HOST="$2"; shift 2;;
-    *) echo "unknown parameter $1." ; exit 1 ; break;;
+    --domain) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else ACCEPT_DOMAIN[${#ACCEPT_DOMAIN[*]}]=$2; shift 2; fi ;;
+    --ip) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else ACCEPT_IP[${#ACCEPT_IP[*]}]=$2; shift 2; fi ;;
+    --email) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else EMAIL="$2"; shift 2; fi ;;
+    --pass) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else PASS="$2"; shift 2; fi ;;
+    --country) if [[ "$2" == -* || -z "$2" ]]; then command_error $1 ; else Country="$2"; shift 2; fi ;;
+    --state) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else State="$2"; shift 2; fi ;;
+    --locality) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else Locality="$2"; shift 2; fi ;;
+    --organization) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else Organization="$2"; shift 2; fi ;;
+    --unit) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else Unit="$2"; shift 2; fi ;;
+    --commonname) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else CommonName="$2"; shift 2; fi ;;
+    --help) shift 1;;
+    -h) shift 1;;
+    *) echo "unknown parameter $1." ; Usage ; break;;
   esac    
 done
+
+if [[ ${#ACCEPT_DOMAIN[*]} == 0 && ${#ACCEPT_IP[*]} == 0 ]]; then
+  echo '请指定 --domain 或 --ip 参数'
+  exit 1
+fi
+
+for domain in ${ACCEPT_DOMAIN[@]}
+do
+  ACCEPT_STRING=${ACCEPT_STRING}DNS:${domain},
+done
+
+for ip in ${ACCEPT_IP[@]}
+do
+  ACCEPT_STRING=${ACCEPT_STRING}IP:${ip},
+done
+
+ACCEPT_STRING=${ACCEPT_STRING}IP:127.0.0.1
+
 # 验证密码是否输入
 if [ -z "${PASS}" ] ;then
   echo "请输入密码 --pass xxxx"
@@ -34,13 +94,8 @@ if [ -z "${EMAIL}" ] ;then
   exit 1
 fi
 
-# 验证主机域名是否输入
-if [ -z "${HOST}" ] ;then
-  echo "请输入主机名称 --host xxxx.xxx.xxx"
-  exit 1
-fi
 
-SUBJ="/C=${Country}/ST=${State}/L=${Locality}/O=${Organization}/OU=${Unit}/CN=${HOST}/emailAddress=${EMAIL}"
+SUBJ="/C=${Country}/ST=${State}/L=${Locality}/O=${Organization}/OU=${Unit}/CN=${CommonName}/emailAddress=${EMAIL}"
 
 # 生成CA证书及服务端秘钥
 SERVER_PATH="./server"
@@ -49,13 +104,13 @@ openssl genrsa -aes256 -passout pass:${PASS} -out ${SERVER_PATH}/ca-key.pem 4096
 openssl req -passin pass:${PASS} -new -x509 -days 365 -key ${SERVER_PATH}/ca-key.pem -sha256 -out ${SERVER_PATH}/ca.pem -subj ${SUBJ}
 
 openssl genrsa -out ${SERVER_PATH}/server-key.pem 4096
-openssl req -subj "/CN=${HOST}" -sha256 -new -key ${SERVER_PATH}/server-key.pem -out ${SERVER_PATH}/server.csr
-echo subjectAltName = DNS:${HOST},IP:127.0.0.1 >> ${SERVER_PATH}/extfile.cnf
+openssl req -subj "/CN=${CommonName}" -sha256 -new -key ${SERVER_PATH}/server-key.pem -out ${SERVER_PATH}/server.csr
+echo subjectAltName = ${ACCEPT_STRING} >> ${SERVER_PATH}/extfile.cnf
 echo extendedKeyUsage = serverAuth >> ${SERVER_PATH}/extfile.cnf
 openssl x509 -passin pass:${PASS} -req -days 365 -sha256 -in ${SERVER_PATH}/server.csr -CA ${SERVER_PATH}/ca.pem -CAkey ${SERVER_PATH}/ca-key.pem -CAcreateserial -out ${SERVER_PATH}/server-cert.pem -extfile ${SERVER_PATH}/extfile.cnf
 
 # 删除过程文件
-rm ${SERVER_PATH}/server.csr ${SERVER_PATH}/extfile.cnf
+rm -f ${SERVER_PATH}/server.csr ${SERVER_PATH}/extfile.cnf ./.srl
 
 chmod -v 0400 ${SERVER_PATH}/ca-key.pem ${SERVER_PATH}/server-key.pem
 chmod -v 0444 ${SERVER_PATH}/ca.pem ${SERVER_PATH}/server-cert.pem
--- Docker科普/scripts/生成连接证书/gen-user.sh
+++ Docker科普/scripts/生成连接证书/gen-user.sh
@@ -1,17 +1,51 @@
-
 #!/usr/bin/env bash
+
+Usage(){
+    cat <<EOF
+
+Usage: $0 <--pass> <--user> [<-t> | <--tar filename>]
+
+生成Docker使用的客户端证书
+
+Requires:
+  --pass string             CA的签名密码
+  --user string             客户端的用户名
+
+Options:
+  -t,--tar filename         将生成的证书打包，包名缺省"${USER}-cert.tar.gz"
+
+EOF
+    exit 1
+}
+
+command_error(){
+  echo "$1 参数值不能为空"
+  exit 1
+}
+
+if [[ "$1" == "-h" || "$1" == "--help" || -z "$1" ]]; then
+  Usage
+fi
 
 
 PASS=
 USER=
+TAR_NAME=
+TAR_ENABLE=
 
-while [ $# -ge 2 ] ; do
+while [ $# -ge 1 ] ; do
   case "$1" in
-    --pass) PASS="$2"; shift 2;;
-    --user) USER="$2"; shift 2;;
-    *) echo "unknown parameter $1." ; exit 1 ; break;;
+    --pass) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else PASS="$2"; shift 2; fi ;;
+    --user) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else USER="$2"; shift 2; fi ;;
+    --tar) if [[ "$2" == -* || -z "$2"  ]]; then command_error $1 ; else TAR_ENABLE=1; TAR_NAME="$2"; shift 2; fi ;;
+    -t) TAR_ENABLE=1; shift 1;;
+    --help) shift 1;;
+    -h) shift 1;;
+    *) echo "unknown parameter $1." ; Usage ; break;;
   esac    
 done
+
+
 # 验证密码是否输入
 if [ -z "${PASS}" ] ;then
   echo "请输入密码 --pass xxxx"
@@ -41,3 +75,12 @@ chmod -v 0444 ${USER_PATH}/cert.pem
 
 # 复制根证书
 cp -f ${SERVER_PATH}/ca.pem ${USER_PATH}/
+
+# 打包
+if [[ ! -z "${TAR_ENABLE}" ]]; then
+  if [[ -z "${TAR_NAME}" ]]; then
+    TAR_NAME=${USER}-cert.tar.gz
+  fi
+  tar -czvf ${TAR_NAME} ./${USER}/*  
+  rm -rf ./${USER}
+fi
